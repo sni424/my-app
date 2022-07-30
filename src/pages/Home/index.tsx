a8e12c019bbc57c7c7c726e3d37302371dd88890
jong <sni424@naver.com>
2022-07-30 23:31:17 +0900

    타입공부

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,10 +20,59 @@ const Home = () => {
     const both: BothType = 20;
 
     console.log(active, hex, arr, both);
+
+    const sayHello = (): void => {
+        console.log("hello");
+    };
+    enum Os {
+        Window = 3,
+        Ios = 10,
+        Android,
+    }
+
+    type Score = "A" | "B" | "C" | "F";
+
+    interface User {
+        name: string;
+        age: number;
+        gender?: string;
+        birthYear: number;
+        [grade: number]: Score;
+    }
+
+    let user: User = {
+        name: "hello",
+        age: 30,
+        birthYear: 2000,
+        1: "A",
+        2: "B",
+    };
+
+    interface Add {
+        (num1: number, numb2: number): number;
+    }
+
+    const interGood: Add = (x, y) => {
+        return x + y;
+    };
+
+    interface IsAdult {
+        (age: number): boolean;
+    }
+
+    const goodT: IsAdult = (age) => {
+        return age > 19;
+    };
+
     useEffect(() => {
         greeter("world");
         greetUser({ name: "love", age: 30 });
         console.log(returnType(4));
+        sayHello();
+        console.log(Os[10], Os["Window"]);
+        console.log(user.name, (user.gender = "his"));
+        console.log(10, 20);
+        console.log(goodT(17));
     }, []);
 
     return (
