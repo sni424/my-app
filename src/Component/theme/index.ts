817eba592c0519affd3f8be280e364308b91a1eb
jong <sni424@naver.com>
2022-08-04 05:10:39 +0900

    typescript styled-component

---
+++ src/Component/theme/index.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    textColor: "whitesmoke",
+    bgColor: "#111",
+    btnColor: "tomato",
+};
+
+export const lightTheme: DefaultTheme = {
+    textColor: "#111",
+    bgColor: "whitesmoke",
+    btnColor: "teal",
+};
---
+++ src/pages/BlockChain/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlockChain = () => {
+    return <div></div>;
+};
+
+export default BlockChain;
